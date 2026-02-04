e2a9a215e57789668e64cc837f7b89df117947ee
Sachin Kumar <sy247917@gmail.com>
2026-02-04 23:47:56 +0530

    Add files via upload

---
+++ calculater advance.py
@@ -0,0 +1,39 @@
+result=int(input("enter the number:"))
+op= input("enter operator(+,-,*,/,=):")
+num=int(input("enter next number:")0
+op= input("enter operator(+,-,*,/,=):")
+if op=="+":
+    result=result+num
+elif op =="-":
+   result=result-num
+elif op=="*":
+   result= result*num
+elif op=="/":
+  result=result/num
+
+calculator_advance.py
+
+result=int(iput("enter the number"))
+ num=none
+while true:
+     op=input("enter operator(+,-,*,/,=):")
+      if op =="=" and num is none:
+         continue
+     if num is not None:
+        if op=="=":
+          print(f"Result={result)")
+          break
+     else:
+        print(f"result={result}")
+        print(f"Next opertion {result}{op}")
+num int(input("enter the next number:")
+  if op =="+":
+     result=result+num
+  elif op=="-":
+      result=result-num
+  elif op=="*":
+      result= result*num
+  elif op=="/":
+      result=resilt/num
+   else;
+      print("enter the valid operator" )
